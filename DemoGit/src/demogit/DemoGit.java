edbf48788a097d54fea1af72d05693c5cf003dd9
vvaisan <vvaisan@TTYHL-C0105.ak.lan>
2013-05-22 10:15:57 +0300

    Branch

--- DemoGit/src/demogit/DemoGit.java
+++ DemoGit/src/demogit/DemoGit.java
@@ -15,6 +15,7 @@ public class DemoGit {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        //Comment 3333
         System.out.println("Hello");
     }
 }
