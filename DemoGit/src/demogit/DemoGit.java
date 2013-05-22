51ed7c1afc7cfbc9dd786a6e7930cceb7116ed0b
vvaisan <vvaisan@TTYHL-C0105.ak.lan>
2013-05-22 10:08:25 +0300

    New saved

---
+++ DemoGit/src/demogit/DemoGit.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package demogit;
+
+/**
+ *
+ * @author vvaisan
+ */
+public class DemoGit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
