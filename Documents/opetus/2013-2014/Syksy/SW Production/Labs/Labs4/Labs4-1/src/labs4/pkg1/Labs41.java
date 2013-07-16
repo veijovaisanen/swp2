284b1a7087894fddc17e0e34fd238b1d3f3d2350
Veijo Väisänen <veijo.vaisanen@oamk.fi>
2013-07-16 12:46:33 +0300

    New

---
+++ Documents/opetus/2013-2014/Syksy/SW Production/Labs/Labs4/Labs4-1/src/labs4/pkg1/Labs41.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package labs4.pkg1;
+
+/**
+ *
+ * @author vvaisan
+ */
+public class Labs41 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
