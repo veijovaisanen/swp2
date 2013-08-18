2deb36934dd022443ff71710bd884d1db1e4aec5
Veijo Väisänen <veijo.vaisanen@oamk.fi>
2013-08-18 17:44:37 +0300

    added

--- swp2/src/swp2/Swp2.java
+++ swp2/src/swp2/Swp2.java
@@ -2,18 +2,19 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package labs4.pkg1;
+package swp2;
 
 /**
  *
  * @author vvaisan
  */
-public class Labs41 {
+public class Swp2 {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
+		//Comment
     }
 }
